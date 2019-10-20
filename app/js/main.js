cf10a4c565930cb7511b357304873eb9e2ac585d
oleg-fit <oleg26-spb@rambler.ru>
2019-10-20 16:45:54 +0300

    Разметка и стили для  блока header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function(){
 
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/header/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/header/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots'
+    });
 
 
-  
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+    });
+
+
 });
