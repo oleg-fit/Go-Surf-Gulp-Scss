8835868c359161f55be5bf4569310822446e26f8
oleg-fit <oleg26-spb@rambler.ru>
2019-10-23 19:37:01 +0300

    Разметка и стили блока Surf

--- app/js/main.js
+++ app/js/main.js
@@ -5,15 +5,28 @@ $(function(){
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/header/arrows-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/header/arrows-right.svg" alt=""></img>',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
 
-
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider'
     });
 
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/header/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/header/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-map'
+    });
 
+    $('.slider-map').slick({
+        slidesToShow: 9,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true
+    });
 });
